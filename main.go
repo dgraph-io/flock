703dc618066a2e553f62d2ea394e7d81c84d174a
Aman Mangal <aman@dgraph.io>
2019-04-01 18:41:20 +0530

    Add reverse index for author predicate

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ id_str: string @index(exact) @upsert .
 created_at: dateTime @index(hour) .
 hashtags: [string] @index(exact) .
 
-author: uid @reverse .
+author: uid @count @reverse .
 mention: uid @reverse .
 `
 
