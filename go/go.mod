f858e110896b063ba8ac045055a496e8952c7aea
Ibrahim Jarif <ibrahim@dgraph.io>
2020-12-19 16:35:28 +0530

    Support downloading tweets to files

--- go/go.mod
+++ go/go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/azr/backoff v0.0.0-20160115115103-53511d3c7330 // indirect
 	github.com/dgraph-io/badger v1.6.0
 	github.com/dgraph-io/dgo/v2 v2.0.0
+	github.com/dgraph-io/ristretto v0.0.4-0.20201218073023-6a5070bff779
+	github.com/dustin/go-humanize v1.0.0
 	github.com/dustin/go-jsonpointer v0.0.0-20160814072949-ba0abeacc3dc // indirect
 	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
 	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
--- go/main.go
+++ go/main.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"bufio"
+	"compress/gzip"
 	"context"
 	"encoding/json"
 	"errors"
@@ -27,15 +28,20 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"os/signal"
+	"path"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"sync/atomic"
+	"syscall"
 	"time"
 
 	"github.com/ChimeraCoder/anaconda"
-	"github.com/dgraph-io/badger/y"
 	"github.com/dgraph-io/dgo/v2"
 	"github.com/dgraph-io/dgo/v2/protos/api"
+	"github.com/dgraph-io/ristretto/z"
+	"github.com/dustin/go-humanize"
 	"google.golang.org/grpc"
 )
 
@@ -90,6 +96,7 @@ const (
 var (
 	opts  progOptions
 	stats progStats
+	fid   uint64
 
 	errNotATweet      = errors.New("message in the stream is not a tweet")
 	errShouldNotReach = errors.New("invariant failed to satisfy")
@@ -103,22 +110,27 @@ type twitterCreds struct {
 }
 
 type progOptions struct {
-	NumClients       int
-	CredentialsFile  string
-	DataFilesPath    string
-	ReportPeriodSecs int
-	NoCommitRatio    float64
-	AlphaSockAddr    []string
+	numClients       int
+	numWriters       int
+	credentialsFile  string
+	dataFilesPath    string
+	reportPeriodSecs int
+	noCommitRatio    float64
+	alphaSockAddr    []string
+	outFilesPath     string
+	fileSize         int
 }
 
 type progStats struct {
-	Tweets        uint32
-	Commits       uint32
-	LeakedCommits uint32
-	Retries       uint32
-	Failures      uint32
-	ErrorsJSON    uint32
-	ErrorsDgraph  uint32
+	TotalDownloaded uint64
+	Tweets          uint32
+	Commits         uint32
+	LeakedCommits   uint32
+	Retries         uint32
+	Failures        uint32
+	ErrorsJSON      uint32
+	ErrorsDgraph    uint32
+	NumFiles        uint32
 }
 
 type twitterUser struct {
@@ -183,7 +195,101 @@ func buildQuery(tweet *twitterTweet) string {
 	return finalQuery
 }
 
-func runInserter(alphas []api.DgraphClient, c *y.Closer, tweets <-chan interface{}) {
+type writer struct {
+	dir   string
+	f     *os.File
+	w     *gzip.Writer
+	fsz   uint64
+	maxSz uint64
+}
+
+func newWriter() *writer {
+	var w writer
+	w.maxSz = uint64(opts.fileSize)
+	w.dir = opts.outFilesPath
+	w.newFiles()
+	return &w
+}
+
+func (w *writer) Write(buf []byte) (int, error) {
+	if w.f == nil {
+		w.newFiles()
+	}
+
+	sz, err := w.w.Write(buf)
+	if err != nil {
+		return sz, err
+	}
+	if atomic.AddUint64(&w.fsz, uint64(sz)) > w.maxSz {
+		w.Finish()
+		w.newFiles()
+	}
+	return sz, nil
+}
+
+func (w *writer) newFiles() {
+	id := atomic.AddUint64(&fid, 1)
+	fname := fmt.Sprintf("%06d", id)
+
+	absDir, err := filepath.Abs(opts.outFilesPath)
+	checkFatal(err, "absdir %s", absDir)
+	path := path.Join(absDir, fname+gzFileSuffix)
+
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+	checkFatal(err, "Failed to open file %s", fname+gzFileSuffix)
+
+	gw := gzip.NewWriter(f)
+	gw.Header.Name = fname + jsonFileSuffix
+
+	w.f, w.w, w.fsz = f, gw, 0
+	atomic.AddUint32(&stats.NumFiles, 1)
+}
+
+func (w *writer) Finish() {
+	if w.f == nil && w.w == nil {
+		return
+	}
+	checkFatal(w.w.Flush(), "writer flush failed")
+	checkFatal(w.w.Close(), "writer close failed")
+	checkFatal(w.f.Close(), "file close failed")
+}
+func runWriter(c *z.Closer, tweets <-chan interface{}) {
+	defer c.Done()
+	w := newWriter()
+	defer func() {
+		w.Finish()
+	}()
+	for {
+		select {
+		case <-c.HasBeenClosed():
+			return
+		case jsn, more := <-tweets:
+			if !more {
+				return
+			}
+			var tweet anaconda.Tweet
+			switch msg := jsn.(type) {
+			case anaconda.Tweet:
+				tweet = msg
+			default:
+				// Not a tweet
+				atomic.AddUint32(&stats.ErrorsJSON, 1)
+			}
+
+			atomic.AddUint32(&stats.Tweets, 1)
+
+			data, err := json.Marshal(tweet)
+			checkFatal(err, "Json marshal failed for %+v", tweet)
+
+			sz, err := w.Write(data)
+			checkFatal(err, "failed to write %s", string(data))
+
+			atomic.AddUint64(&stats.TotalDownloaded, uint64(sz))
+		}
+	}
+}
+
+func runInserter(alphas []api.DgraphClient, c *z.Closer, tweets <-chan interface{}) {
 	defer c.Done()
 
 	if tweets == nil {
@@ -223,7 +329,7 @@ func runInserter(alphas []api.DgraphClient, c *y.Closer, tweets <-chan interface
 			}
 
 			commitNow := true
-			if rand.Float64() < opts.NoCommitRatio {
+			if rand.Float64() < opts.noCommitRatio {
 				commitNow = false
 			}
 
@@ -275,6 +381,13 @@ func filterTweet(jsn interface{}) (*twitterTweet, error) {
 		return nil, errNotATweet
 	}
 
+	x, err := json.Marshal(tweet)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(x))
+
+	// fmt.Println(tweet)
 	createdAt, err := time.Parse(cTimeFormat, tweet.CreatedAt)
 	if err != nil {
 		return nil, err
@@ -364,28 +477,70 @@ func newAPIClients(sockAddr []string) []api.DgraphClient {
 	return clients
 }
 
-// TODO: fix the race condition here
-func reportStats(c *y.Closer) {
+func reportWriteStats(c *z.Closer) {
 	defer c.Done()
 
 	var oldStats, newStats progStats
-	log.Printf("Reporting stats every %v seconds\n", opts.ReportPeriodSecs)
-	for {
-		newStats = stats
-		log.Printf("STATS tweets: %d, commits: %d, leaked: %d, json_errs: %d, "+
-			"retries: %d, failures: %d, dgraph_errs: %d, "+
-			"commit_rate: %d/sec\n",
-			newStats.Tweets, newStats.Commits, newStats.LeakedCommits, newStats.ErrorsJSON,
-			newStats.Retries, newStats.Failures, newStats.ErrorsDgraph,
-			(newStats.Tweets-oldStats.Tweets)/uint32(opts.ReportPeriodSecs))
-		oldStats = newStats
+	ticker := time.NewTicker(time.Duration(opts.reportPeriodSecs) * time.Second)
+	defer ticker.Stop()
 
+	log.Printf("Reporting stats every %v seconds\n", opts.reportPeriodSecs)
+	for {
 		select {
 		case <-c.HasBeenClosed():
 			return
-		default:
-			time.Sleep(time.Second * time.Duration(opts.ReportPeriodSecs))
+		case <-ticker.C:
 		}
+		newStats = stats
+
+		tweets := atomic.LoadUint32(&newStats.Tweets)
+		errorsJSON := atomic.LoadUint32(&newStats.ErrorsJSON)
+		numFiles := atomic.LoadUint32(&newStats.NumFiles)
+		totalDownloaded := atomic.LoadUint64(&newStats.TotalDownloaded)
+
+		oldTweets := atomic.LoadUint32(&oldStats.Tweets)
+		oldSz := atomic.LoadUint64(&oldStats.TotalDownloaded)
+
+		rate := humanize.IBytes(uint64((totalDownloaded - oldSz) / uint64(opts.reportPeriodSecs)))
+
+		log.Printf("STATS tweets: %d, json_errs: %d, created files: %d "+
+			"download_rate: %d tweets/sec download_speed: %s/sec Total: %s\n", tweets, errorsJSON, numFiles,
+			(tweets-oldTweets)/uint32(opts.reportPeriodSecs), rate, humanize.IBytes(totalDownloaded))
+
+		oldStats = newStats
+	}
+}
+
+func reportInsertStats(c *z.Closer) {
+	defer c.Done()
+
+	var oldStats, newStats progStats
+	ticker := time.NewTicker(time.Duration(opts.reportPeriodSecs) * time.Second)
+	defer ticker.Stop()
+
+	log.Printf("Reporting stats every %v seconds\n", opts.reportPeriodSecs)
+	for {
+		select {
+		case <-c.HasBeenClosed():
+			return
+		case <-ticker.C:
+		}
+		newStats = stats
+		tweets := atomic.LoadUint32(&newStats.Tweets)
+		commits := atomic.LoadUint32(&newStats.Commits)
+		leakedCommits := atomic.LoadUint32(&newStats.LeakedCommits)
+		errorsJSON := atomic.LoadUint32(&newStats.ErrorsJSON)
+		retries := atomic.LoadUint32(&newStats.Retries)
+		failures := atomic.LoadUint32(&newStats.Failures)
+		errorsDgraph := atomic.LoadUint32(&newStats.ErrorsDgraph)
+
+		oldCommits := atomic.LoadUint32(&oldStats.Commits)
+		log.Printf("STATS tweets: %d, commits: %d, leaked: %d, json_errs: %d, "+
+			"retries: %d, failures: %d, dgraph_errs: %d, "+"commit_rate: %d/sec\n",
+			tweets, commits, leakedCommits, errorsJSON, retries, failures, errorsDgraph,
+			(commits-oldCommits)/uint32(opts.reportPeriodSecs))
+
+		oldStats = newStats
 	}
 }
 
@@ -398,26 +553,128 @@ func checkFatal(err error, format string, args ...interface{}) {
 }
 
 func main() {
-	dgclients := flag.Int("l", 8, "number of dgraph clients to run")
-	credentialsFile := flag.String("c", "credentials.json", "path to credentials file")
-	dataFilesPath := flag.String("d", "", "path containing json files with tweets in each line")
-	noCommitRatio := flag.Float64("p", 0, "prob of CommitNow=False, from 0.0 to 1.0")
+	flag.IntVar(&opts.numClients, "l", 1, "number of dgraph clients to run")
+	flag.IntVar(&opts.numWriters, "num-writers", 4, "number of writer to run")
+	flag.StringVar(&opts.credentialsFile, "c", "credentials.json", "path to credentials file")
+	flag.StringVar(&opts.dataFilesPath, "d", "", "path containing json files with tweets in each line")
+	flag.StringVar(&opts.outFilesPath, "o", "./data", "Directory to store compressed json tweets")
+	flag.Float64Var(&opts.noCommitRatio, "p", 0, "prob of CommitNow=False, from 0.0 to 1.0")
+	flag.IntVar(&opts.fileSize, "fsz", 512, "Max size of the generated gz file (in MB)")
+
 	alphasAddress := flag.String("a", ":9180,:9182,:9183", "comma separated addresses to alphas")
+	downloadTweets := flag.Bool("download", false, "Download tweets and save to directory specified by -o flag")
+
 	flag.Parse()
 
-	if *noCommitRatio > 1 || *noCommitRatio < 0 {
+	if opts.noCommitRatio > 1 || opts.noCommitRatio < 0 {
 		log.Fatalf("invalid value for commit=false probability")
 	}
-	opts = progOptions{
-		NumClients:       *dgclients,
-		CredentialsFile:  *credentialsFile,
-		DataFilesPath:    *dataFilesPath,
-		ReportPeriodSecs: 2,
-		NoCommitRatio:    *noCommitRatio,
-		AlphaSockAddr:    strings.Split(*alphasAddress, ","),
+	opts.reportPeriodSecs = 2
+	opts.fileSize = opts.fileSize << 20
+	opts.alphaSockAddr = strings.Split(*alphasAddress, ",")
+
+	var tweetChannel chan interface{}
+	if opts.dataFilesPath == "" {
+		creds := readCredentials(opts.credentialsFile)
+		client := newTwitterClient(creds)
+		stream := client.PublicStreamSample(nil)
+		tweetChannel = stream.C
+		defer stream.Stop()
+	} else {
+		tweetChannel = setupChannelFromDir(opts.dataFilesPath)
 	}
 
-	alphas := newAPIClients(opts.AlphaSockAddr)
+	if *downloadTweets {
+		startWriters(tweetChannel)
+	} else {
+		startInserters(tweetChannel)
+	}
+}
+
+var (
+	gzFileSuffix   = "tweets.gz"
+	jsonFileSuffix = "tweets.json"
+)
+
+func parseFid(name string) uint64 {
+	name = path.Base(name)
+	if !strings.HasSuffix(name, gzFileSuffix) {
+		return 0
+	}
+	switch {
+	case strings.HasSuffix(name, gzFileSuffix):
+		name = strings.TrimSuffix(name, gzFileSuffix)
+	case strings.HasSuffix(name, jsonFileSuffix):
+		name = strings.TrimSuffix(name, jsonFileSuffix)
+	default:
+		log.Fatalf("Unknown file suffix %s", name)
+	}
+	name = strings.TrimSuffix(name, gzFileSuffix)
+	id, err := strconv.Atoi(name)
+	if err != nil {
+		return 0
+	}
+	if id <= 0 {
+		log.Fatal("Id %d should be greater than 0", id)
+	}
+	return uint64(id)
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+func startWriters(tweetChannel <-chan interface{}) {
+	cl := z.NewCloser(1)
+	go reportWriteStats(cl)
+	numWriters := opts.numWriters
+	log.Printf("Using %d writers\n", numWriters)
+
+	sdCh := make(chan os.Signal, 1)
+
+	exit := z.NewCloser(1)
+	// sigint : Ctrl-C, sigterm : kill command.
+	signal.Notify(sdCh, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		defer exit.Done()
+		<-sdCh
+		log.Printf("Caught Ctrl-C. Terminating now (this may take a few seconds)...")
+		cl.SignalAndWait()
+	}()
+	dirExists, err := exists(opts.outFilesPath)
+	checkFatal(err, "Dir exist check failed for %s", opts.outFilesPath)
+	if !dirExists {
+		checkFatal(os.MkdirAll(opts.outFilesPath, 0777), "failed to create %s dir", opts.outFilesPath)
+	}
+
+	dInfo, err := ioutil.ReadDir(opts.outFilesPath)
+	checkFatal(err, "ReadDir %s", opts.outFilesPath)
+
+	fid = uint64(0)
+	for _, i := range dInfo {
+		id := parseFid(i.Name())
+		if id > fid {
+			fid = id
+		}
+	}
+	log.Println("Found Max fid:", fid)
+
+	// read twitter stream
+	cl.AddRunning(numWriters)
+	for i := 0; i < numWriters; i++ {
+		go runWriter(cl, tweetChannel)
+	}
+	exit.Wait()
+}
+func startInserters(tweetChannel <-chan interface{}) {
+	alphas := newAPIClients(opts.alphaSockAddr)
 
 	// setup schema
 	dgr := dgo.NewDgraphClient(alphas...)
@@ -441,25 +698,13 @@ func main() {
 	}
 
 	// report stats
-	r := y.NewCloser(1)
-	go reportStats(r)
-	log.Printf("Using %v dgraph clients on %v alphas\n",
-		opts.NumClients, len(opts.AlphaSockAddr))
-
-	var tweetChannel chan interface{}
-	if opts.DataFilesPath == "" {
-		creds := readCredentials(opts.CredentialsFile)
-		client := newTwitterClient(creds)
-		stream := client.PublicStreamSample(nil)
-		tweetChannel = stream.C
-		defer stream.Stop()
-	} else {
-		tweetChannel = setupChannelFromDir(opts.DataFilesPath)
-	}
+	r := z.NewCloser(1)
+	go reportInsertStats(r)
+	log.Printf("Using %v dgraph clients on %v alphas\n", opts.numClients, len(opts.alphaSockAddr))
 
 	// read twitter stream
-	c := y.NewCloser(0)
-	for i := 0; i < opts.NumClients; i++ {
+	c := z.NewCloser(0)
+	for i := 0; i < opts.numClients; i++ {
 		c.AddRunning(1)
 		go runInserter(alphas, c, tweetChannel)
 	}
